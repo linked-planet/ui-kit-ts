dc426e3cad2e66f2b6770969befc26e8049b2a0f
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-01-08 09:31:00 +0100

    updated packages

--- showcase/src/components/showcase/wrapper/LayeringShowcase.tsx
+++ showcase/src/components/showcase/wrapper/LayeringShowcase.tsx
@@ -2,29 +2,58 @@ import React from "react"
 import ShowcaseWrapperItem, {
 	ShowcaseProps,
 } from "../../ShowCaseWrapperItem/ShowcaseWrapperItem"
+import { Input } from "@linked-planet/ui-kit-ts"
 
 //#region layeringexample
 function Example() {
 	return (
 		<div className="bg-surface hover:bg-surface-hovered active:bg-surface-pressed border-border border p-4">
 			bg-surface
+			<div className="p-4">
+				<Input placeholder="Test Input" />
+				<Input disabled placeholder="Test Input Disabled" />
+			</div>
 			<div className="bg-surface-raised hover:bg-surface-raised-hovered active:bg-surface-raised-pressed border-border border p-4">
 				bg-surface-raised
+				<div className="p-4">
+					<Input placeholder="Test Input" />
+					<Input disabled placeholder="Test Input Disabled" />
+				</div>
 				<div className="bg-surface-overlay hover:bg-surface-overlay-hovered active:bg-surface-overlay-pressed border-border border p-4">
 					bg-surface-overlay
+					<div className="p-4">
+						<Input placeholder="Test Input" />
+						<Input disabled placeholder="Test Input Disabled" />
+					</div>
 				</div>
 			</div>
 			<div className="bg-surface-sunken border-border mt-4 border p-4">
 				bg-surface-sunken
+				<div className="p-4">
+					<Input placeholder="Test Input" />
+					<Input disabled placeholder="Test Input Disabled" />
+				</div>
 			</div>
 			<div className="border-border shadow-raised mt-4 border p-4">
 				shadow-raised
+				<div className="p-4">
+					<Input placeholder="Test Input" />
+					<Input disabled placeholder="Test Input Disabled" />
+				</div>
 			</div>
 			<div className="border-border shadow-overflow mt-4 border p-4">
 				shadow-overflow
+				<div className="p-4">
+					<Input placeholder="Test Input" />
+					<Input disabled placeholder="Test Input Disabled" />
+				</div>
 			</div>
 			<div className="border-border shadow-overlay mt-4 border p-4">
 				shadow-overlay
+				<div className="p-4">
+					<Input placeholder="Test Input" />
+					<Input disabled placeholder="Test Input Disabled" />
+				</div>
 			</div>
 		</div>
 	)
