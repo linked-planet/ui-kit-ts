c174a76325eeb2fb783e5360d7ff8b5402c6f480
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-01-24 17:30:53 +0100

    removed button outline on focus

--- library/src/components/Button.tsx
+++ library/src/components/Button.tsx
@@ -99,7 +99,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 				className={twMerge(
 					"relative box-border flex flex-shrink-0 items-center justify-center gap-1 rounded border border-transparent px-3 py-1.5 outline-1 outline-offset-2",
 					!disabled ? ButtonStyles[appearance] : undefined,
-					`${appearance !== "subtle" ? "disabled:bg-disabled" : ""} disabled:text-disabled-text focus:outline-brand-hovered disabled:cursor-not-allowed`,
+					`${appearance !== "subtle" ? "disabled:bg-disabled" : ""} disabled:text-disabled-text disabled:cursor-not-allowed`,
 					selected ? ButtonSelectedStyles : undefined,
 					className,
 				)}
