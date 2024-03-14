bba5cb5b00652b0d60b2638436ef932ad64b6e54
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-03-14 16:38:50 +0100

    WIP Calendar + DatePicker

--- library/src/components/Calendar.tsx
+++ library/src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useEffect, useMemo, useState } from "react"
 
 import {
 	DayPicker,
@@ -7,78 +7,20 @@ import {
 	type DayPickerMultipleProps,
 	type DayPickerRangeProps,
 	type DayPickerProps,
+	ActiveModifiers,
 } from "react-day-picker"
 
 import ChevronLeftLargeIcon from "@atlaskit/icon/glyph/chevron-left-large"
 import ChevronRightLargeIcon from "@atlaskit/icon/glyph/chevron-right-large"
+import { dateFromString, formatToDateType } from "../utils/DateUtils"
+import dayjs from "dayjs"
 
 //import "react-day-picker/dist/style.css" -> is imported in index.ts of the library that it is before TW
 
-type CalendarDefaultProps = Pick<
+export type DateType = `${number}-${number}-${number}`
+
+/*type CalendarDefaultProps = Pick<
 	DayPickerDefaultProps,
-	| "mode"
-	| "defaultMonth"
-	| "onMonthChange"
-	| "disableNavigation"
-	| "locale"
-	| "month"
-	| "onDayClick"
-	| "onNextClick"
-	| "onPrevClick"
-	| "selected"
-	| "today"
-	| "weekStartsOn"
-	| "id"
-	| "className"
-	| "style"
-	| "disabled"
-	| "title"
-> & { testId?: string }
-
-type CalendarSingleProps = Pick<
-	DayPickerSingleProps,
-	| "mode"
-	| "defaultMonth"
-	| "onMonthChange"
-	| "disableNavigation"
-	| "locale"
-	| "month"
-	| "onDayClick"
-	| "onNextClick"
-	| "onPrevClick"
-	| "selected"
-	| "today"
-	| "weekStartsOn"
-	| "id"
-	| "className"
-	| "style"
-	| "disabled"
-	| "title"
->
-
-type CalendarMultipleProps = Pick<
-	DayPickerMultipleProps,
-	| "mode"
-	| "defaultMonth"
-	| "onMonthChange"
-	| "disableNavigation"
-	| "locale"
-	| "month"
-	| "onDayClick"
-	| "onNextClick"
-	| "onPrevClick"
-	| "selected"
-	| "today"
-	| "weekStartsOn"
-	| "id"
-	| "className"
-	| "style"
-	| "disabled"
-	| "title"
->
-
-type CalendarRangeProps = Pick<
-	DayPickerRangeProps,
 	| "mode"
 	| "defaultMonth"
 	| "onMonthChange"
@@ -98,15 +40,111 @@ type CalendarRangeProps = Pick<
 	| "title"
 	| "showOutsideDays"
 	| "fixedWeeks"
->
+	| "labels"
+> & {
+	testId?: string
+	nextMonthLabel?: string
+	previousMonthLabel?: string
+	todayLabel?: string
+}*/
 
-const captionStyles = "flex justify-center items-center relative"
+type CalendarSingleProps = Pick<
+	DayPickerSingleProps,
+	| "mode"
+	| "defaultMonth"
+	| "onMonthChange"
+	| "disableNavigation"
+	| "locale"
+	| "month"
+	| "onDayClick"
+	| "onNextClick"
+	| "onPrevClick"
+	| "selected"
+	| "onSelect"
+	| "today"
+	| "weekStartsOn"
+	| "id"
+	| "className"
+	| "style"
+	| "disabled"
+	| "showOutsideDays"
+	| "fixedWeeks"
+	| "title"
+	| "labels"
+> & {
+	testId?: string
+	nextMonthLabel?: string
+	previousMonthLabel?: string
+	todayLabel?: string
+}
+
+type CalendarMultipleProps = Pick<
+	DayPickerMultipleProps,
+	| "mode"
+	| "defaultMonth"
+	| "onMonthChange"
+	| "disableNavigation"
+	| "locale"
+	| "month"
+	| "onDayClick"
+	| "onNextClick"
+	| "onPrevClick"
+	| "selected"
+	| "onSelect"
+	| "today"
+	| "weekStartsOn"
+	| "id"
+	| "className"
+	| "style"
+	| "disabled"
+	| "title"
+	| "showOutsideDays"
+	| "fixedWeeks"
+	| "labels"
+> & {
+	testId?: string
+	nextMonthLabel?: string
+	previousMonthLabel?: string
+	todayLabel?: string
+}
+
+type CalendarRangeProps = Pick<
+	DayPickerRangeProps,
+	| "mode"
+	| "defaultMonth"
+	| "onMonthChange"
+	| "disableNavigation"
+	| "locale"
+	| "month"
+	| "onDayClick"
+	| "onNextClick"
+	| "onPrevClick"
+	| "selected"
+	| "onSelect"
+	| "today"
+	| "weekStartsOn"
+	| "id"
+	| "className"
+	| "style"
+	| "disabled"
+	| "title"
+	| "showOutsideDays"
+	| "fixedWeeks"
+	| "labels"
+> & {
+	testId?: string
+	nextMonthLabel?: string
+	previousMonthLabel?: string
+	todayLabel?: string
+}
+
+const captionStyles = "flex justify-center items-center relative w-full"
 const captionLabelStyles = "text-text text-sm font-bold flex justify-center"
 const daySelectedStyles =
-	"rounded-none bg-selected hover:bg-selected-hovered active:bg-selected-pressed text-text-inverse font-bold w-full h-full"
+	"rounded-none bg-selected hover:bg-selected-hovered active:bg-selected-pressed text-selected-text-inverse font-bold w-full h-full"
 const headStyles = "text-text-subtle text-sm border-b-0"
 const dayTodayStyles =
-	"font-bold relative text-brand-text aria-selected:text-text-inverse after:absolute after:block after:left-1 after:right-1 after:bottom-1 after:bg-brand-text after:aria-selected:bg-text-inverse after:h-[2px]"
+	"font-bold relative text-brand-text aria-selected:text-text-inverse after:absolute after:block after:left-1 after:right-1 after:bg-brand-text after:aria-selected:bg-text-inverse after:h-[2px]"
 const navStyles =
 	"whitespace-nowrap absolute w-full flex justify-between items-center"
 
@@ -127,39 +165,330 @@ const classNames: DayPickerProps["classNames"] = {
 	day_disabled: "text-disabled-text",
 	day_outside: "text-disabled-text",
 	day_today: dayTodayStyles,
-	root: "p-1",
+	root: "p-1 w-max",
 	tbody: "border-b-0",
 }
 
-//const styles: DayPickerProps["styles"] = {}
-
-export function Calendar(props: CalendarDefaultProps): JSX.Element
-export function Calendar(props: CalendarSingleProps): JSX.Element
-export function Calendar(props: CalendarMultipleProps): JSX.Element
-export function Calendar(props: CalendarRangeProps): JSX.Element
-export function Calendar(
-	props: (
-		| CalendarDefaultProps
-		| CalendarSingleProps
-		| CalendarMultipleProps
-		| CalendarRangeProps
-	) & { testId?: string },
+export function CalendarBase(props: CalendarSingleProps): JSX.Element
+export function CalendarBase(props: CalendarMultipleProps): JSX.Element
+export function CalendarBase(props: CalendarRangeProps): JSX.Element
+export function CalendarBase(
+	props: CalendarSingleProps | CalendarMultipleProps | CalendarRangeProps,
 ): React.ReactNode {
-	const showOutsideDays =
-		"showOutsideDays" in props ? props.showOutsideDays : true
+	const nextMonthLabel = props.nextMonthLabel || "Next Month"
+	const previousMonthLabel = props.previousMonthLabel || "Previous Month"
 
-	const fixedWeeks = "fixedWeeks" in props ? props.fixedWeeks : true
 	return (
 		<DayPicker
 			data-testid={props.testId}
 			classNames={classNames}
-			showOutsideDays={showOutsideDays}
-			fixedWeeks={fixedWeeks}
+			showOutsideDays={props.showOutsideDays ?? true}
+			fixedWeeks={props.fixedWeeks ?? true}
 			components={{
-				IconLeft: () => <ChevronLeftLargeIcon label="Previous Month" />,
-				IconRight: () => <ChevronRightLargeIcon label="Next Month" />,
+				IconLeft: () => (
+					<ChevronLeftLargeIcon label={previousMonthLabel} />
+				),
+				IconRight: () => (
+					<ChevronRightLargeIcon label={nextMonthLabel} />
+				),
 			}}
 			{...props}
 		/>
 	)
+}
+
+type AKCompatibleProps = {
+	id?: string
+	testId?: string
+	mode?: "single" | "multiple" | "range"
+	key?: string
+	month?: number
+	defaultMonth?: number
+	year?: number
+	defaultYear?: number
+	defaultSelected?: { from: DateType; to: DateType }
+	defaultPreviouslySelected?: { from: DateType; to: DateType }
+	selected?: { from: DateType; to: DateType }
+	previouslySelected?: { from: DateType; to: DateType }
+	disabledDates?: DateType[]
+	disabledDateFilter?: (date: DateType) => boolean
+	//locale?: string
+	minDate?: DateType
+	maxDate?: DateType
+	nextMonthLabel?: string
+	previousMonthLabel?: string
+	/* 0 = Sunday, 1 = Monday, 2 = Tuesday, 3 = Wednesday, 4 = Thursday, 5 = Friday, 6 = Saturday */
+	weekStartDay?: 0 | 1 | 2 | 3 | 4 | 5 | 6
+	/* shows days outside of the month */
+	showOutsideDays?: boolean
+	/* always show 6 weeks, requires show outside day to be true */
+	fixedWeeks?: boolean
+
+	onDayClick?: (date: DateType, activeModifiers: ActiveModifiers) => void
+	onSelectionChanged?: (selected: { from: DateType; to: DateType }) => void
+}
+
+export function Calendar({
+	id,
+	testId,
+	key,
+	mode,
+	month: monthProp,
+	defaultMonth,
+	year: yearProp,
+	defaultYear,
+	defaultSelected,
+	defaultPreviouslySelected,
+	selected,
+	previouslySelected,
+	disabledDates,
+	disabledDateFilter,
+	//locale: localeString,
+	minDate: minDateProp,
+	maxDate: maxDateProp,
+	nextMonthLabel,
+	previousMonthLabel,
+	weekStartDay,
+	onDayClick,
+	onSelectionChanged,
+	fixedWeeks,
+	showOutsideDays,
+}: AKCompatibleProps) {
+	const [month, setMonth] = useState<dayjs.Dayjs>(dayjs().startOf("month"))
+	const [selectedDates, setSelectedDates] = useState<DateType[]>()
+
+	useEffect(() => {
+		if (mode === "single") {
+		}
+	}, [mode])
+
+	const minDate = useMemo(
+		() =>
+			minDateProp
+				? dayjs(dateFromString(minDateProp, true)).startOf("month")
+				: undefined,
+		[minDateProp],
+	)
+	const maxDate = useMemo(
+		() =>
+			maxDateProp
+				? dayjs(dateFromString(maxDateProp, true)).startOf("month")
+				: undefined,
+		[maxDateProp],
+	)
+
+	useEffect(() => {
+		let month = monthProp
+			? dayjs()
+					.month(monthProp - 1)
+					.startOf("month")
+			: defaultMonth
+				? dayjs()
+						.month(defaultMonth - 1)
+						.startOf("month")
+				: dayjs().startOf("month")
+		if (yearProp) {
+			if (month) {
+				month = month.year(yearProp)
+			} else {
+				month = dayjs().year(yearProp)
+			}
+		} else if (defaultYear) {
+			if (month) {
+				month = month.year(defaultYear)
+			} else {
+				month = dayjs().year(defaultYear)
+			}
+		}
+		month = month?.startOf("month")
+
+		if (minDate && month && month.isBefore(minDate, "month")) {
+			month = minDate
+		}
+		if (maxDate && month && month.isAfter(maxDate, "month")) {
+			month = maxDate
+		}
+
+		setMonth((curr) => {
+			if (
+				curr.month() === month.month() &&
+				curr.year() === month.year()
+			) {
+				return curr
+			}
+			return month
+		})
+	}, [
+		defaultMonth,
+		defaultYear,
+		maxDate,
+		minDate,
+		month,
+		monthProp,
+		yearProp,
+	])
+
+	const _onDayClick = useCallback(
+		(day: Date, activeModifiers: ActiveModifiers) => {
+			const dateType = formatToDateType(day)
+
+			setSelectedDates((selectedDates) => {
+				if (mode === "single") {
+					if (
+						selectedDates.length === 1 &&
+						selectedDates[0] === dateType
+					) {
+						onSelectionChanged?.([])
+						return []
+					}
+					onSelectionChanged?.([dateType])
+					return [dateType]
+				}
+
+				if (mode === "range") {
+					if (selectedDates.length === 1) {
+						if (selectedDates[0] === dateType) {
+							onSelectionChanged?.([])
+							return []
+						}
+						const from = dateFromString(selectedDates[0], true)
+						const to = day
+						if (from.getTime() > to.getTime()) {
+							onSelectionChanged?.([dateType, selectedDates[0]])
+							return [dateType, selectedDates[0]]
+						}
+						onSelectionChanged?.([selectedDates[0], dateType])
+						return [selectedDates[0], dateType]
+					}
+					onSelectionChanged?.([dateType])
+					return [dateType]
+				}
+
+				const idx = selectedDates.indexOf(dateType)
+				if (idx !== -1) {
+					const updated = [...selectedDates]
+					updated.splice(idx, 1)
+					onSelectionChanged?.(updated)
+					return updated
+				}
+				const updated = [...selectedDates, dateType]
+				onSelectionChanged?.(updated)
+				return updated
+			})
+
+			onDayClick?.(dateType, activeModifiers)
+		},
+		[mode, onDayClick, onSelectionChanged],
+	)
+
+	const disabledMatcher = useCallback(
+		(dateD: Date) => {
+			const date = dayjs(dateD)
+			if (minDate && date.isBefore(minDate, "day")) {
+				return true
+			}
+			if (maxDate && date.isAfter(maxDate, "day")) {
+				return true
+			}
+			if (
+				(disabledDates && disabledDates.length > 0) ||
+				disabledDateFilter
+			) {
+				const dateType = formatToDateType(date)
+				if (disabledDates?.includes(dateType)) {
+					return true
+				}
+				if (disabledDateFilter) {
+					return disabledDateFilter(dateType)
+				}
+			}
+			return false
+		},
+		[disabledDateFilter, disabledDates, maxDate, minDate],
+	)
+
+	const _selectedDates = useMemo(
+		() => selectedDates.map((d) => dateFromString(d, true)),
+		[selectedDates],
+	)
+
+	return useMemo(() => {
+		switch (mode) {
+			case "single":
+				return (
+					<CalendarBase
+						id={id}
+						key={key}
+						month={month.toDate()}
+						testId={testId}
+						disabled={disabledMatcher}
+						//locale={locale}
+						onDayClick={_onDayClick}
+						weekStartsOn={weekStartDay}
+						nextMonthLabel={nextMonthLabel}
+						previousMonthLabel={previousMonthLabel}
+						fixedWeeks={fixedWeeks}
+						showOutsideDays={showOutsideDays}
+						mode="single"
+						selected={_selectedDates[0]}
+					/>
+				)
+
+			case "multiple":
+				return (
+					<CalendarBase
+						id={id}
+						key={key}
+						month={month.toDate()}
+						testId={testId}
+						disabled={disabledMatcher}
+						//locale={locale}
+						onDayClick={_onDayClick}
+						weekStartsOn={weekStartDay}
+						nextMonthLabel={nextMonthLabel}
+						previousMonthLabel={previousMonthLabel}
+						fixedWeeks={fixedWeeks}
+						showOutsideDays={showOutsideDays}
+						mode="multiple"
+						selected={_selectedDates}
+					/>
+				)
+			case "range":
+				return (
+					<CalendarBase
+						id={id}
+						key={key}
+						month={month.toDate()}
+						testId={testId}
+						disabled={disabledMatcher}
+						//locale={locale}
+						onDayClick={_onDayClick}
+						weekStartsOn={weekStartDay}
+						nextMonthLabel={nextMonthLabel}
+						previousMonthLabel={previousMonthLabel}
+						fixedWeeks={fixedWeeks}
+						showOutsideDays={showOutsideDays}
+						mode="range"
+						selected={{
+							from: _selectedDates[0],
+							to: _selectedDates[1],
+						}}
+					/>
+				)
+		}
+	}, [
+		_onDayClick,
+		_selectedDates,
+		disabledMatcher,
+		fixedWeeks,
+		id,
+		key,
+		mode,
+		month,
+		nextMonthLabel,
+		previousMonthLabel,
+		showOutsideDays,
+		testId,
+		weekStartDay,
+	])
 }
