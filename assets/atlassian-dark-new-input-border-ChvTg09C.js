3a69ce4a8b713309d242e114b9a7b0142626740c
marcus-wishes <marcus-wishes@users.noreply.github.com>
2024-03-26 13:10:29 +0000

    Deploying to docs from @ linked-planet/ui-kit-ts@7c46d147c147b8ea98587e7d208e770711e4895d 🚀

---
+++ assets/atlassian-dark-new-input-border-ChvTg09C.js
@@ -0,0 +1,7 @@
+const a=`
+html[data-color-mode="light"][data-theme~="light:dark"],
+html[data-color-mode="dark"][data-theme~="dark:dark"] {
+  color-scheme: dark;
+  --ds-border-input: #738496;
+}
+`;export{a as default};
---
+++ assets/atlassian-light-new-input-border-D3KLe-gi.js
@@ -0,0 +1,7 @@
+const t=`
+html[data-color-mode="light"][data-theme~="light:light"],
+html[data-color-mode="dark"][data-theme~="dark:light"] {
+  color-scheme: light;
+  --ds-border-input: #8590A2;
+}
+`;export{t as default};
