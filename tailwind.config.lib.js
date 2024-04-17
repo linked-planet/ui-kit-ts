a548f7426bd48cbbac0bdc1b8146858dcf470ccb
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-04-17 11:05:03 +0200

    tw removed preflight bundling

--- tailwind.config.lib.js
+++ tailwind.config.lib.js
@@ -1,5 +1,11 @@
 import { theme } from "./twThemes/twThemeAK"
 export default {
+	/** disable the css reset "preflight" */
+	corePlugins: {
+		preflight: false,
+	},
+	/** create classes with !important to avoid overwrites because of name clashes */
+	important: true,
 	content: ["./library/src/**/*.tsx", "./library/src/**/*.ts"],
 	theme,
 	plugins: [],
