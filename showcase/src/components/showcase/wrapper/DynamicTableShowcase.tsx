05c773a81ee039f48b493ae1d80a8669eef5d6a4
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-07-15 15:44:45 +0200

    removed old AK dynamic table example

--- showcase/src/components/showcase/wrapper/DynamicTableShowcase.tsx
+++ showcase/src/components/showcase/wrapper/DynamicTableShowcase.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode, useMemo } from "react"
+import React, { type ReactNode, useMemo } from "react"
 import ShowcaseWrapperItem, {
-	ShowcaseProps,
+	type ShowcaseProps,
 } from "../../ShowCaseWrapperItem/ShowcaseWrapperItem"
 import DynamicTable from "@atlaskit/dynamic-table"
 import Button from "@atlaskit/button"
@@ -12,112 +12,6 @@ import {
 	Label,
 	Checkbox,
 } from "@linked-planet/ui-kit-ts"
-
-//#region table-example
-function AKDynamicTableExample() {
-	return (
-		<div style={{ minWidth: 300 }}>
-			<DynamicTable
-				caption=""
-				head={{
-					cells: [
-						{
-							key: "first",
-							content: "First col",
-							isSortable: true,
-						},
-						{
-							key: "second",
-							content: "Second col",
-							isSortable: true,
-						},
-						{ key: "third", content: <span>Action col</span> },
-					],
-				}}
-				rowsPerPage={3}
-				rows={[
-					{
-						key: "1stRow",
-						cells: [
-							{ content: "1-1" },
-							{ content: "1-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-					{
-						key: "2ndRow",
-						cells: [
-							{ content: "2-1" },
-							{ content: "2-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-					{
-						key: "3rdRow",
-						cells: [
-							{ content: "3-1" },
-							{ content: "3-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-					{
-						key: "4thRow",
-						cells: [
-							{ content: "4-1" },
-							{ content: "4-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-					{
-						key: "5thRow",
-						cells: [
-							{ content: "5-1" },
-							{ content: "5-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-					{
-						key: "6thRow",
-						cells: [
-							{ content: "6-1" },
-							{ content: "6-2" },
-							{
-								content: (
-									<Button appearance="primary">Delete</Button>
-								),
-							},
-						],
-					},
-				]}
-				onSort={(item) => console.log("Sorting", item)}
-				onSetPage={(pageNumber) =>
-					console.log("SetPageNumber", pageNumber)
-				}
-			></DynamicTable>
-		</div>
-	)
-}
-//#endregion table-example
 
 //#region datatable-example
 type RowData = {
@@ -271,11 +165,6 @@ function DynamicTableShowcase(props: ShowcaseProps) {
 				},
 			]}
 			examples={[
-				{
-					title: "AK Dynamic Table",
-					example: <AKDynamicTableExample />,
-					sourceCodeExampleId: "table-example",
-				},
 				{
 					title: "Data Table",
 					example: <DataTableExample />,
