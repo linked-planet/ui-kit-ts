ae5b9e8cc36cdd70179ecf5a3104424d71d9b911
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-07-16 10:48:37 +0200

    updated text field show case to be forwarded to the input one

--- showcase/src/components/showcase/wrapper/TextFieldShowcase.tsx
+++ showcase/src/components/showcase/wrapper/TextFieldShowcase.tsx
@@ -1,45 +1,30 @@
 import React from "react"
 import ShowcaseWrapperItem, {
-	ShowcaseProps,
+	type ShowcaseProps,
 } from "../../ShowCaseWrapperItem/ShowcaseWrapperItem"
-import TextField from "@atlaskit/textfield"
 
 function TextFieldShowcase(props: ShowcaseProps) {
 	//#region textfield
-	const example1 = (
-		<div style={{ minWidth: 300 }}>
-			<TextField defaultValue="Content of text field..." />
-		</div>
-	)
 	//#endregion textfield
-	//#region textfield2
-	const example2 = (
-		<div style={{ minWidth: 300 }}>
-			<TextField defaultValue="Password" type="password" />
-		</div>
-	)
-	//#endregion textfield2
 
 	return (
 		<ShowcaseWrapperItem
 			name="Text field"
+			description="Please use the Input component instead."
 			{...props}
 			packages={[
 				{
-					name: "@atlaskit/textfield",
-					url: "https://atlassian.design/components/textfield/examples",
+					name: "@linked-planet/ui-kit-ts",
+					url: "https://linked-planet.github.io/ui-kit-ts/single?component=Input",
 				},
 			]}
 			examples={[
 				{
-					title: "Example",
-					example: example1,
+					title: "Input Link",
+					example: (
+						<a href="single?component=Input">Open Input Page</a>
+					),
 					sourceCodeExampleId: "textfield",
-				},
-				{
-					title: "Example 2",
-					example: example2,
-					sourceCodeExampleId: "textfield2",
 				},
 			]}
 		/>
