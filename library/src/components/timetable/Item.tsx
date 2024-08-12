a651161d057cb3cb88f6ca0f58d476be29194ab2
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2024-08-12 23:01:17 +0200

    time table - example item styling

--- library/src/components/timetable/Item.tsx
+++ library/src/components/timetable/Item.tsx
@@ -1,11 +1,11 @@
 import type { TimeSlotBooking, TimeTableGroup } from "./TimeTable"
-
-import styles from "./Item.module.css"
 import type { TimeTableItemProps } from "./ItemWrapper"
+import { twJoin } from "tailwind-merge"
 
 export function Item({
 	item,
 	selectedItem,
+	height,
 }: TimeTableItemProps<TimeTableGroup, TimeSlotBooking>): JSX.Element {
 	const isSelected = selectedItem === item
 	const title = `${item.title}:\n${item.startDate.format(
@@ -13,10 +13,13 @@ export function Item({
 	)} -\n${item.endDate.format("HH:mm DD-MM-YY")}`
 	return (
 		<div
-			className={`${styles.timeSlotItem} ${
-				isSelected ? styles.selected : ""
-			}`}
+			className={twJoin(
+				"box-border flex flex-1 w-full justify-center leading-8 bg-success-bold text-text relative z-[1] m-0 truncate border-solid border-2 rounded",
+				"active:bg-success-bold-pressed hover:bg-selected-bold",
+				isSelected ? "border-selected-border" : "border-success-border",
+			)}
 			title={title}
+			style={{ height }}
 		>
 			<p className="truncate">{item.title ?? "no name"}</p>
 		</div>
