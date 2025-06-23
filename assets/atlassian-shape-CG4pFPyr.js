4dfdb52b73a180658adb7614565aee04e052d164
marcus-wishes <marcus-wishes@users.noreply.github.com>
2025-06-23 11:34:27 +0000

    Deploying to docs from @ linked-planet/ui-kit-ts@0a24b13b6e5ffa82522f1e158c946c4a77e9972e 🚀

---
+++ assets/atlassian-shape-CG4pFPyr.js
@@ -0,0 +1,15 @@
+const r=`
+html[data-theme~="shape:shape"] {
+  --ds-border-radius-050: 0.125rem;
+  --ds-border-radius: 0.25rem;
+  --ds-border-radius-100: 0.25rem;
+  --ds-border-radius-200: 0.5rem;
+  --ds-border-radius-300: 0.75rem;
+  --ds-border-radius-400: 1rem;
+  --ds-border-radius-circle: 2002rem;
+  --ds-border-width: 0.0625rem;
+  --ds-border-width-0: 0rem;
+  --ds-border-width-indicator: 0.1875rem;
+  --ds-border-width-outline: 0.125rem;
+}
+`;export{r as default};
