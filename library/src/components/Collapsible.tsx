98ff2c00f3bab7f33519db9994bad24aae0ff1b0
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2025-07-15 16:29:27 +0200

    Collapsible - new Structure/base-ui implementation

--- library/src/components/Collapsible.tsx
+++ library/src/components/Collapsible.tsx
@@ -1,151 +1,150 @@
-import * as CollapsibleRUI from "@radix-ui/react-collapsible"
+import { Collapsible as CollapsibleRUI } from "@base-ui-components/react/collapsible"
 
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react"
 
-import { forwardRef, type HTMLProps } from "react"
+import { forwardRef, useCallback } from "react"
 import { twMerge } from "tailwind-merge"
 
-type CollapsibleProps = {
-	open?: boolean
-	defaultOpen?: boolean
-	onChanged?: (opened: boolean) => void
-	triggerTitle?: string
-	header: React.ReactNode
-	triggerDisabled?: boolean
-	triggerClassName?: string
-	triggerStyle?: React.CSSProperties
+type TriggerProps = CollapsibleRUI.Trigger.Props & {
+	openButtonPosition?: "left" | "right" | "hidden"
 	chevronClassName?: string
 	chevronStyle?: React.CSSProperties
 	headerContainerStyle?: React.CSSProperties
 	headerContainerClassName?: string
-	contentClassName?: string
-	contentStyle?: React.CSSProperties
-	className?: string
-	style?: React.CSSProperties
-	children: React.ReactNode
-	openButtonPosition?: "left" | "right" | "hidden"
-	id?: string
-	testId?: string
-	/**
-	 * If true, the content will be mounted even when the collapsible is closed.
-	 * This is useful if you want to keep the content in the DOM even when it is not visible.
-	 */
-	forceMountContent?: true
-} & HTMLProps<HTMLDivElement>
+}
+type PanelProps = CollapsibleRUI.Panel.Props & {
+	transitionDuration?: string
+}
 
-export const Collapsible = forwardRef(
+type CollapsibleProps = CollapsibleRUI.Root.Props
+
+function Trigger({
+	className,
+	openButtonPosition = "left",
+	chevronClassName,
+	chevronStyle,
+	headerContainerStyle,
+	headerContainerClassName,
+	title,
+	children,
+	...props
+}: TriggerProps) {
+	const classNameResolved = useCallback(
+		(state: CollapsibleRUI.Root.State) => {
+			const basicClassName = `flex w-full flex-1 items-center bg-surface-raised hover:bg-surface-raised-hovered active:bg-surface-raised-pressed justify-start select-none border border-border border-solid group-data-[closed]:rounded-xs group-data-[open]:rounded-t-xs ${
+				openButtonPosition === "hidden"
+					? "cursor-default"
+					: "cursor-pointer disabled:cursor-default"
+			}`
+			if (typeof className === "function") {
+				return twMerge(basicClassName, className(state))
+			}
+			return twMerge(basicClassName, className)
+		},
+		[className, openButtonPosition],
+	)
+	return (
+		<CollapsibleRUI.Trigger className={classNameResolved} {...props}>
+			<div className="flex items-center w-full justify-between">
+				{openButtonPosition === "left" && (
+					<button
+						type="button"
+						className={twMerge(
+							"flex h-full flex-none items-center justify-center size-6 pr-1",
+							chevronClassName,
+						)}
+						title={title}
+					>
+						<ChevronRightIcon
+							strokeWidth={3}
+							className="group-data-[closed]:rotate-0 group-data-[open]:rotate-90 transform transition-transform"
+						/>
+					</button>
+				)}
+				<div
+					className={twMerge(
+						"flex w-full flex-1 justify-start",
+						headerContainerClassName,
+					)}
+					style={headerContainerStyle}
+				>
+					{children}
+				</div>
+				{openButtonPosition === "right" && (
+					<button
+						type="button"
+						className={twMerge(
+							"flex h-full flex-none items-center justify-center size-6",
+							chevronClassName,
+						)}
+						style={chevronStyle}
+						title={title}
+					>
+						<ChevronLeftIcon
+							strokeWidth={3}
+							className="group-data-[closed]:rotate-0 group-data-[open]:-rotate-90 transform transition-transform"
+						/>
+					</button>
+				)}
+			</div>
+		</CollapsibleRUI.Trigger>
+	)
+}
+
+function Panel({ className, ...props }: PanelProps) {
+	const classNameResolved = useCallback(
+		(state: CollapsibleRUI.Root.State) => {
+			const basicClassName =
+				"overflow-hidden h-[var(--collapsible-panel-height)] data-[starting-style]:h-0 data-[ending-style]:h-0 origin-top data-[open]:border-x data-[open]:border-b bg-surface data-[open]:border-border border-solid transition-all ease-linear motion-reduce:transition-none"
+			if (typeof className === "function") {
+				return twMerge(basicClassName, className(state))
+			}
+			return twMerge(basicClassName, className)
+		},
+		[className],
+	)
+
+	return (
+		<CollapsibleRUI.Panel
+			keepMounted={props.keepMounted ?? true}
+			hiddenUntilFound={props.hiddenUntilFound ?? true}
+			className={classNameResolved}
+			{...props}
+		/>
+	)
+}
+
+export const Root = forwardRef(
 	(
-		{
-			open: opened,
-			defaultOpen = false,
-			onChanged,
-			openButtonPosition = "left",
-			triggerTitle = "Collapsible Toggle",
-			header,
-			triggerDisabled,
-			triggerClassName,
-			triggerStyle,
-			headerContainerStyle,
-			headerContainerClassName,
-			chevronClassName,
-			chevronStyle,
-			contentClassName,
-			contentStyle,
-			className,
-			style,
-			children,
-			id,
-			testId,
-			forceMountContent = undefined,
-			...props
-		}: CollapsibleProps,
+		{ className, children, ...props }: CollapsibleProps,
 		ref: React.ForwardedRef<HTMLDivElement>,
 	) => {
+		const classNameResolved = useCallback(
+			(state: CollapsibleRUI.Root.State) => {
+				const basicClassName = "bg-surface-raised rounded-xs group"
+				if (typeof className === "function") {
+					return twMerge(basicClassName, className(state))
+				}
+				return twMerge(basicClassName, className)
+			},
+			[className],
+		)
+
 		return (
 			<CollapsibleRUI.Root
 				{...props}
-				className={twMerge(
-					"bg-surface-raised rounded-xs group",
-					className,
-				)}
-				style={style}
-				data-testid={testId}
-				id={id}
+				className={classNameResolved}
 				ref={ref}
-				open={opened}
-				defaultOpen={defaultOpen}
-				onOpenChange={(open) => onChanged?.(open)}
 			>
-				<CollapsibleRUI.Trigger
-					className={twMerge(
-						`flex p-1.5 flex-1 items-center hover:bg-surface-raised-hovered active:bg-surface-raised-pressed justify-start select-none ${
-							openButtonPosition === "hidden"
-								? "cursor-default"
-								: "cursor-pointer disabled:cursor-default"
-						}`,
-						triggerClassName,
-					)}
-					style={triggerStyle}
-					asChild
-				>
-					<div>
-						{openButtonPosition === "left" && (
-							<button
-								type="button"
-								className={twMerge(
-									"flex h-full flex-none items-center justify-center size-6 pr-1",
-									chevronClassName,
-								)}
-								title={triggerTitle}
-							>
-								<ChevronRightIcon
-									aria-label={opened ? "close" : "open"}
-									strokeWidth={3}
-									className="group-data-[state=closed]:rotate-0 group-data-[state=open]:rotate-90 transform transition-transform"
-								/>
-							</button>
-						)}
-						<div
-							className={twMerge(
-								"flex w-full flex-1 justify-start",
-								headerContainerClassName,
-							)}
-							style={headerContainerStyle}
-						>
-							{header}
-						</div>
-						{openButtonPosition === "right" && (
-							<button
-								type="button"
-								className={twMerge(
-									"flex h-full flex-none items-center justify-center size-6",
-									chevronClassName,
-								)}
-								style={chevronStyle}
-								title={triggerTitle}
-							>
-								<ChevronLeftIcon
-									aria-label={opened ? "close" : "open"}
-									strokeWidth={3}
-									className="group-data-[state=closed]:rotate-0 group-data-[state=open]:-rotate-90 transform transition-transform"
-								/>
-							</button>
-						)}
-					</div>
-				</CollapsibleRUI.Trigger>
-
-				<CollapsibleRUI.Content
-					className={twMerge(
-						"overflow-hidden data-[state=closed]:animate-slide-up-collapsible data-[state=open]:animate-slide-down-collapsible data-[state=closed]:h-0",
-						contentClassName,
-					)}
-					style={contentStyle}
-					forceMount={forceMountContent}
-				>
-					{children}
-				</CollapsibleRUI.Content>
+				{children}
 			</CollapsibleRUI.Root>
 		)
 	},
 )
-Collapsible.displayName = "Collapsible"
+Root.displayName = "CollapsibleRoot"
+
+export const Collapsible = {
+	Root,
+	Trigger,
+	Content: Panel,
+}
