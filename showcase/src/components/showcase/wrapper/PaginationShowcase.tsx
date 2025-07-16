bad0da89c28424646d08385b3a7b699720cca176
Markus T. <19794318+marcus-wishes@users.noreply.github.com>
2025-07-16 18:19:06 +0200

    Enhance Pagination Component - Added next/previous page handlers and button disabling
    - Introduced `onNextPage` and `onPreviousPage` props for better navigation control.
    - Added `disableNextPageButton` and `disablePreviousPageButton` props to manage button states.
    - Updated logic to handle undefined pages and improve user experience in pagination.

--- showcase/src/components/showcase/wrapper/PaginationShowcase.tsx
+++ showcase/src/components/showcase/wrapper/PaginationShowcase.tsx
@@ -17,20 +17,18 @@ function PageExample() {
 	)
 
 	return (
-		<>
-			<Pagination
-				pages={pages}
-				currentPage={selectedPage}
-				onPageChange={(page) => setSelectedPage(page)}
-				totalPages={10}
-				//maxPageButtons={10}
-				pageSize={pageSize}
-				pageSizes={[10, 20, 50, 100]}
-				onPageSizeChange={setPageSize}
-				pageSizeMenuSide="top"
-				pageSizeMenuAlign="end"
-			/>
-		</>
+		<Pagination
+			pages={pages}
+			currentPage={selectedPage}
+			onPageChange={(page) => setSelectedPage(page)}
+			totalPages={10}
+			//maxPageButtons={10}
+			pageSize={pageSize}
+			pageSizes={[10, 20, 50, 100]}
+			onPageSizeChange={setPageSize}
+			pageSizeMenuSide="top"
+			pageSizeMenuAlign="end"
+		/>
 	)
 	//#endregion pagination
 }
@@ -62,6 +60,34 @@ function IndexExample() {
 	//#endregion pagination-index
 }
 
+function SimpleExample() {
+	//#region pagination-simple
+	const [pageSize, setPageSize] = useState(10)
+	const [currentPage, setCurrentPage] = useState(1)
+
+	return (
+		<Pagination
+			onNextPage={() => {
+				console.log("next")
+				setCurrentPage(currentPage + 1)
+			}}
+			onPreviousPage={() => {
+				console.log("previous")
+				setCurrentPage(currentPage - 1)
+			}}
+			disableNextPageButton={currentPage >= 10}
+			disablePreviousPageButton={currentPage <= 1}
+			pageSize={pageSize}
+			currentPage={currentPage}
+			pageSizes={[10, 20, 50, 100]}
+			onPageSizeChange={setPageSize}
+			pageSizeMenuSide="top"
+			pageSizeMenuAlign="end"
+		/>
+	)
+	//#endregion pagination-simple
+}
+
 function PaginationShowcase(props: ShowcaseProps) {
 	return (
 		<ShowcaseWrapperItem
@@ -83,6 +109,11 @@ function PaginationShowcase(props: ShowcaseProps) {
 					title: "Example Index",
 					example: <IndexExample />,
 					sourceCodeExampleId: "pagination-index",
+				},
+				{
+					title: "Minimal",
+					example: <SimpleExample />,
+					sourceCodeExampleId: "pagination-simple",
 				},
 			]}
 		/>
