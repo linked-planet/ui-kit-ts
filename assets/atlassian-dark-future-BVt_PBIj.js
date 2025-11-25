3af7f147ba668216c28d5f10b16ccfe57b8159fb
marcus-wishes <marcus-wishes@users.noreply.github.com>
2025-11-25 12:20:41 +0000

    Deploying to docs from @ linked-planet/ui-kit-ts@7a73ad12039af2ee53a293f517f7dc191e69e3a5 🚀

---
+++ assets/atlassian-dark-future-BVt_PBIj.js
@@ -0,0 +1,7 @@
+const a=`
+html[data-color-mode="light"][data-theme~="light:light"],
+html[data-color-mode="dark"][data-theme~="dark:light"] {
+  color-scheme: dark;
+  --ds-background-disabled: #B3DF72;
+}
+`;export{a as default};
